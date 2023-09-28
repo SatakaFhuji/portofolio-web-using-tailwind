3e07fd3816070f696e8e308ee75b45b4d035b4db
SatakaFhuji <novaldipurnomo@gmail.com>
2023-09-28 23:55:02 +0700

    3 modified: dist/js/script.js, dist/output.css, index.html

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,17 @@
 window.onscroll = () => {
     const header = document.querySelector('header');
     const fixNav = header.offsetTop;
+    const backToTop = document.querySelector('#back-to-top');
+
 
     if(window.pageYOffset > fixNav) {
         header.classList.add('navbar-fixed')
+        backToTop.classList.remove('hidden')
+        backToTop.classList.add('flex')
     }else {
         header.classList.remove('navbar-fixed')
+        backToTop.classList.add('hidden')
+        backToTop.classList.remove('flex')
     }
 }
 
@@ -18,3 +24,6 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
+
+// click on html element navbar menu in mobile phone gone
+window.addEventListener('click', e => {if (e.target != hamburger && e.target != navMenu) {hamburger.classList.remove('hamburger-active');navMenu.classList.add('hidden');}})
